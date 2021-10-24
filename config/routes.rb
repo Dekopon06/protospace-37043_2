e3b3494228a2c77067594d2de4a0e4afa9e2eb7c
Dekopon06 <92018465+Dekopon06@users.noreply.github.com>
2021-10-24 17:05:41 +0900

    ログイン・新規投稿画面の実装
    resistration/sessionのビューファイルの紐付け

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   get 'prototypes/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # ↑↑上の記述は削除しましょう↑↑
+
   root to: "prototypes#index"
+
+  
+
 end
