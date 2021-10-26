3b4f69f5170e58e111d7b68b7b9d9963ec846f85
Dekopon06 <92018465+Dekopon06@users.noreply.github.com>
2021-10-27 00:55:16 +0900

    リダイレクト処理の記述

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,25 +1,55 @@
 class PrototypesController < ApplicationController
-  # before_action :move_to_index, except: [:index, :show]
-  def index
+  before_action :move_to_index, except: [:index, :new]
+    # before_action :move_to_index, except: [:index, :show]
+   def index
     # user = User.find(params[id])
     # params.require(:tweet).permit(:name)
     # @name = user_name
+   end
+
+   def new
+     @prototype = Prototype.new
+   end
+
+   def create
+     @prototype = Prototype.create(prototype_params[:id])
+    #  @prototype = Prototype.new(prototype_params[:id])
+     if @prototype.save
+      #  Prototype.create(prototype_params[:id])
+      # current_user.create
+      # (prototype_params[:id])
+      redirect_to root_path
+    else
+      render :new
+    end
+   end
+
+
+   private
+
+   def prototype_params
+     params.require(:prototype).permit( :title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+   end
+
+   def configure_permitted_parameters
+    d.permit(:sign_up, keys: [:user_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:profile])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:occupation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:position])
   end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
+
 end
 
-# private
 
-def prototype_params
-  params.require(:prototype).permit(:content, :image).merge(user_id: current_user.id)
-end
 
-def new
-  # @prototype = Prototype.new
-end
-
-def create
-  # @hoge = Huga
-end
 
 # def move_to_index
 #   unless user_signed_in?
--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -7,6 +7,5 @@ class Prototype < ApplicationRecord
   validates :title, presence: true
   validates :catch_copy, presence: true
   validates :concept, presence: true
-  validates :position, presence: true
 
 end
