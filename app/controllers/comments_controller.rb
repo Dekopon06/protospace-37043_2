30e35ad42710faf828f5211362f88ed94a6c20a5
Dekopon06 <92018465+Dekopon06@users.noreply.github.com>
2021-10-30 19:33:20 +0900

    authenticate_user!を用いて制限を設けた

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,48 +2,32 @@ class CommentsController < ApplicationController
   # def create
   #   Comment.create(comment_params)
   # end
- def create
+  def create
    @comment = Comment.new(comment_params)
 
   if @comment.save
      redirect_to prototype_path(@comment.prototype)
   else
-     
-     @prototype = Prototype.find(params[:prototype_id])
-     @comment = Comment.new
-     @comments = @prototype.comments.includes(:user)
+  
+  @prototype = Prototype.find(params[:prototype_id])
+   @comment = Comment.new
+   @comments = @prototype.comments.includes(:user)
 
-     render template: "prototypes/show"
-   # @prototype = [0]
-   # @prototype = Prototype.find(params[:id])
-   # @comment.content(user.user_id)
-   # @prototype = @comment.prototype
-   # @comments = @prototype.comments.includes(:user)
-   # @prototype = @comment.prototype
-   # @comments = @prototype.commentsshoe
+  render template: "prototypes/show"
   end
-end
-#   @comment = Comment.new(comment.params)
-#   if @comment.save
-#     redirect_to prototype_path(@comment.prototype)
-#   else
-#     render :edit
-#  end
+ end
+
  
   private
  def comment_params
   params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
  end
 end
-  # def create
-  #   @comment = Comment.new(comment_params)
-  #   if @comment.save
-  #     redirect_to tweet_path(@comment.tweet) # 今回の実装には関係ありませんが、このようにPrefixでパスを指定することが望ましいです。
-  #   else
-  #     @tweet = @comment.tweet
-  #     @comments = @tweet.comments
-  #     render "tweets/show" # views/tweets/show.html.erbのファイルを参照しています。
-  #   end
-  # end
 
-
+# @prototype = [0]
+# @prototype = Prototype.find(params[:id])
+# @comment.content(user.user_id)
+# @prototype = @comment.prototype
+# @comments = @prototype.comments.includes(:user)
+# @prototype = @comment.prototype
+# @comments = @prototype.commentsshoe
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   get 'prototypes/index'
   root to: "prototypes#index"
-  resources :prototypes do
-    resources :comments, only: :create
-  end
+
+    resources :prototypes do
+      resources :comments, only: :create
+    end
+
+    resources :users, only: :show
+
   #  resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
-  #   resources :comments, only: [:index, :create]
- 
-  resources :users, only: :show
- 
+  #   resources :comments, only: [:index, :create] 
 end
